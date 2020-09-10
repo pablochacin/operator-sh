d446ac5ee8a3c962a7ff8cf325a96ae0c59ac4bf
Pablo Chacin <pchacin@suse.com>
2020-09-10 19:08:43 +0200

    Use test runner in operator unit tests
    Signed-off-by: Pablo Chacin <pchacin@suse.com>

--- tests/operator_test.sh
+++ tests/operator_test.sh
@@ -4,99 +4,130 @@ source lib/test.sh
 source operator.sh
 
 # Test invalid argument option
-unit_test "parse_args --invalid-option"
-assert_command_rc 1
-assert_output_contains "Invalid parameter"
-assert_output_contains "--invalid-option"
-assert_output_contains "Usage"
+function test_invalid_argument(){
+    unit_test "parse_args --invalid-option"
+    assert_command_rc 1
+    assert_output_contains "Invalid parameter"
+    assert_output_contains "--invalid-option"
+    assert_output_contains "Usage"
+}
 
 # Test missing required argument
-unit_test "parse_args"
-assert_command_rc 1
-assert_output_contains "Missing argument"
-assert_output_contains "Object type"
-assert_output_contains "Usage"
+function test_missing_object_type(){
+    unit_test "parse_args"
+    assert_command_rc 1
+    assert_output_contains "Missing argument"
+    assert_output_contains "Object type"
+    assert_output_contains "Usage"
+}
 
 # Test object type is parsed
-MY_OBJECT="my-object"
-unit_test "parse_args -o $MY_OBJECT"
-assert_command_rc 0
-assert_output_contains "OBJECT_TYPE=$MY_OBJECT" 
+function test_object_type_is_parsed(){
+    MY_OBJECT="my-object"
+    unit_test "parse_args -o $MY_OBJECT"
+    assert_command_rc 0
+    assert_output_contains "OBJECT_TYPE=$MY_OBJECT" 
+}
 
 # Test queue name is parsed
-QUEUE_NAME="/tmp/$(uuidgen)"
-unit_test "parse_args -o my-object --queue $QUEUE_NAME"
-assert_command_rc 0
-assert_output_contains "EVENT_QUEUE=$QUEUE_NAME"
+function test_queue_name_is_parsed(){
+    QUEUE_NAME="/tmp/$(uuidgen)"
+    unit_test "parse_args -o my-object --queue $QUEUE_NAME"
+    assert_command_rc 0
+    assert_output_contains "EVENT_QUEUE=$QUEUE_NAME"
+}
 
 # Test namespace is parsed
-MY_NAMESPACE="my-namespace"
-unit_test "parse_args -o my-object -n $MY_NAMESPACE"
-assert_command_rc 0
-assert_output_contains "NAMESPACE=$MY_NAMESPACE"
+function test_namespace_is_parsed(){
+    MY_NAMESPACE="my-namespace"
+    unit_test "parse_args -o my-object -n $MY_NAMESPACE"
+    assert_command_rc 0
+    assert_output_contains "NAMESPACE=$MY_NAMESPACE"
+}
 
 # Test kubeconfig is parsed
-MY_KUBECONFIG="/path/to/my/kubeconfig"
-unit_test "parse_args -o my-object -k $MY_KUBECONFIG"
-assert_command_rc 0
-assert_output_contains "KUBECONFIG=$MY_KUBECONFIG"
+function test_kubeconfig_is_parsed(){
+    MY_KUBECONFIG="/path/to/my/kubeconfig"
+    unit_test "parse_args -o my-object -k $MY_KUBECONFIG"
+    assert_command_rc 0
+    assert_output_contains "KUBECONFIG=$MY_KUBECONFIG"
+}
 
 # Test changes-only is parsed
-unit_test "parse_args -o my-object --changes-only"
-assert_command_rc 0
-assert_output_contains "CHANGES_ONLY=true"
+function test_changes_only_is_parsed(){
+    unit_test "parse_args -o my-object --changes-only"
+    assert_command_rc 0
+    assert_output_contains "CHANGES_ONLY=true"
+}
 
-# Test reset-queuw is parsed
-unit_test "parse_args -o my-object --reset-queue"
-assert_command_rc 0
-assert_output_contains "RESET_QUEUE=true"
+# Test reset-queue is parsed
+function test_reset_queue_is_parsed(){
+    unit_test "parse_args -o my-object --reset-queue"
+    assert_command_rc 0
+    assert_output_contains "RESET_QUEUE=true"
+}
 
 # Test log-events is parsed
-unit_test "parse_args -o my-object --log-events"
-assert_command_rc 0
-assert_output_contains "LOG_EVENTS=true"
+function test_log_events_is_parsed(){
+    unit_test "parse_args -o my-object --log-events"
+    assert_command_rc 0
+    assert_output_contains "LOG_EVENTS=true"
+}
 
 # Test log-file is parsed
-LOG_FILE="/path/to/log/file"
-unit_test "parse_args -o my-object --log-file $LOG_FILE"
-assert_command_rc 0
-assert_output_contains "LOG_FILE=$LOG_FILE"
+function test_log_file_is_parsed(){
+    LOG_FILE="/path/to/log/file"
+    unit_test "parse_args -o my-object --log-file $LOG_FILE"
+    assert_command_rc 0
+    assert_output_contains "LOG_FILE=$LOG_FILE"
+}
 
 # Test reset-log is parsed
-unit_test "parse_args -o my-object --reset-log"
-assert_command_rc 0
-assert_output_contains "RESET_LOG=true"
+function test_reset_log_is_parsed(){
+    unit_test "parse_args -o my-object --reset-log"
+    assert_command_rc 0
+    assert_output_contains "RESET_LOG=true"
+}
 
 # Test log-level is parsed
-unit_test "parse_args -o my-object --log-level DEBUG"
-assert_command_rc 0
-assert_output_contains "LOG_LEVEL=$LOG_LEVEL_DEBUG"
+function test_log_level_is_parsed(){
+    unit_test "parse_args -o my-object --log-level DEBUG"
+    assert_command_rc 0
+    assert_output_contains "LOG_LEVEL=$LOG_LEVEL_DEBUG"
+}
 
 # Test filter-spec is parsed
-unit_test "parse_args -o my-object --filter-spec"
-assert_command_rc 0
-assert_output_contains "FILTER_SPEC=true"
+function test_filter_spec_is_parsed(){
+    unit_test "parse_args -o my-object --filter-spec"
+    assert_command_rc 0
+    assert_output_contains "FILTER_SPEC=true"
+}
 
 # Test filter-status is parsed
-unit_test "parse_args -o my-object --filter-status"
-assert_command_rc 0
-assert_output_contains "FILTER_STATUS=true"
+function test_filter_status_is_parsed(){
+    unit_test "parse_args -o my-object --filter-status"
+    assert_command_rc 0
+    assert_output_contains "FILTER_STATUS=true"
+}
 
 # Test default queue is created
-eval $(parse_args -o OBJECT)
-assert_not_null $EVENT_QUEUE
-unit_test "create_queue"
-assert_command_rc 0
-assert_file_exists $EVENT_QUEUE
+function test_default_queue_is_created(){
+    eval $(parse_args -o OBJECT)
+    assert_not_null $EVENT_QUEUE
+    unit_test "create_queue"
+    assert_command_rc 0 
+    assert_file_exists $EVENT_QUEUE
+}
 
 # Test queue with given name is created
-QUEUE_NAME="/tmp/queue-$(uuidgen)"
-eval $(parse_args -o OBJECT -q $QUEUE_NAME)
-assert_not_null $EVENT_QUEUE
-unit_test "create_queue"
-assert_command_rc 0
-assert_file_exists $QUEUE_NAME
-rm -f $QUEUE_NAME
+function test_named_queue_is_created(){
+    QUEUE_NAME="/tmp/queue-$(uuidgen)"
+    eval $(parse_args -o OBJECT -q $QUEUE_NAME)
+    assert_not_null $EVENT_QUEUE
+    unit_test "create_queue"
+    assert_command_rc 0 
+    assert_file_exists $QUEUE_NAME
+    rm -f $QUEUE_NAME
+}
 
-echo "succeeded"
-
+test_runner
